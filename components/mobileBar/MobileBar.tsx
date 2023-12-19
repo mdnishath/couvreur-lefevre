86fc8e98ef392851e7000f0de2ce15c4663a94a1
Nishath <wpprodeveloper@gmail.com>
2023-12-19 17:54:48 +0600

    mobile bar issue solve

--- components/mobileBar/MobileBar.tsx
+++ components/mobileBar/MobileBar.tsx
@@ -4,7 +4,7 @@ import { SlCallOut } from "react-icons/sl";
 
 const MobileBar = () => {
   return (
-    <div className="md:hidden fixed bottom-0 left-0 w-full bg-primary py-3 shadow-xl">
+    <div className="md:hidden fixed bottom-0 left-0 w-full bg-primary py-3 shadow-xl z-[100]">
       <div className="flex items-center justify-center">
         <h3 className="text-2xl font-semibold text-white">Call Us Today </h3>
         <Link href={`tel:+330662436856`}>
