6c2ecb15ac25d91f601e9214bce47e9ce2998b98
Nishath <wpprodeveloper@gmail.com>
2023-12-19 18:03:39 +0600

    shadow updated

--- components/mobileBar/MobileBar.tsx
+++ components/mobileBar/MobileBar.tsx
@@ -4,16 +4,17 @@ import { SlCallOut } from "react-icons/sl";
 
 const MobileBar = () => {
   return (
-    <div className="md:hidden fixed bottom-0 left-0 w-full bg-primary py-3 shadow-xl z-[100]">
-      <div className="flex items-center justify-center">
-        <h3 className="text-2xl font-semibold text-white">Call Us Today </h3>
-        <Link href={`tel:+330662436856`}>
+    <div className="md:hidden fixed bottom-0 left-0 w-full bg-primary py-3 shadow-top z-[100]">
+      <Link href={`tel:+330662436856`}>
+        <div className="flex items-center justify-center">
+          <h3 className="text-2xl font-semibold text-white">Call Us Today </h3>
+
           <Button className="text-2xl text-white flex gap-2">
             <SlCallOut className="text-2xl" />
             662.436.856
           </Button>
-        </Link>
-      </div>
+        </div>
+      </Link>
     </div>
   );
 };
